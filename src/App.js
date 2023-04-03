a512d83f922569d391fd88f7448709e1475c0e19
rodrigohrzm <rherranzm@gmail.com>
2023-04-03 07:54:02 +0200

    basic presentation imported

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { Presentation } from './pages/Presentation'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit this file to begin your project.
-        </p>
-      </header>
-    </div>
+    <><Presentation/></>
   );
 }
 
