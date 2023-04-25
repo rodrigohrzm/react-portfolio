c7811608244117912a3bf81806806f711523f01f
rodrigohrzm <rherranzm@gmail.com>
2023-04-25 07:58:12 +0200

    first Mantine implementation

--- src/index.js
+++ src/index.js
@@ -7,23 +7,23 @@ import { BrowserRouter, useLocation} from 'react-router-dom';
 import {useLayoutEffect} from 'react';
 import { MantineProvider } from '@mantine/core';
 
-const Wrapper = ({children}) => {
+const ScrollToTop = ({children}) => {
   const location = useLocation();
   useLayoutEffect(() => {
     document.documentElement.scrollTo(0, 0);
   }, [location.pathname]);
   return children
-} 
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Wrapper>
+    <ScrollToTop>
      <MantineProvider withGlobalStyles withNormalizeCSS>
       <App />
      </MantineProvider>
-    </Wrapper>
+    </ScrollToTop>
     </BrowserRouter>
   </React.StrictMode>
 );
