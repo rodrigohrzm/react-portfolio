7ac74eff9c9350f613005a120a35684e6c178d5a
rodrigohrzm <rherranzm@gmail.com>
2023-05-04 21:44:55 +0200

    navigation & login functionality DONE

--- src/pages/webdemo/WebNav.js
+++ src/pages/webdemo/WebNav.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink, Outlet, useNavigate } from "react-router-dom"
 import { submitAPI } from "../../services/FetchAPI";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { useDisclosure } from '@chakra-ui/react'
-import { Box, Flex, Spacer, VStack, HStack, Button, Image, Collapse, Drawer, DrawerOverlay, DrawerContent, DrawerHeader, DrawerBody, FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react'
-
-// falta un navbar link a la dashboard if logged in
-// form validation con el codigo del little lemon
+import { Box, Flex, Spacer, VStack, HStack,
+         Button, Image,
+         Collapse, Drawer, DrawerOverlay, DrawerContent, DrawerHeader, DrawerBody,
+         FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react'
 
 function WebNav() {
+  const navigate = useNavigate();
+
   const {
     isOpen: isOpenNav,
     onClose: onCloseNav,
@@ -23,30 +25,38 @@ function WebNav() {
   onToggle: onToggleLogin
   } = useDisclosure()
 
-  const [ username , setUsername ] = useState ("");
-  const [ password , setPassword ] = useState ("");
   const [ guestButtons , setguestButtons ] = useState ("initial");
   const [ userButtons , setuserButtons ] = useState ("none");
-  const navigate = useNavigate();
+  const [ email , setEmail ] = useState ("");
+  const [ password , setPassword ] = useState ("");
+
+  useEffect(() => {
+    const savedLoginStatus = localStorage.getItem('loginStatus');
+    if (savedLoginStatus === 'true') {
+      userIsLoggedIn(true);
+    }
+  }, []);
 
   const handleLoginSubmit = (event) => {
     event.preventDefault();
-    let formData = [username, password];
+    let formData = [email, password];
     if (submitAPI(formData) === true) {
         onCloseLogin()
         onCloseNav()
         userIsLoggedIn(true)
-        navigate("/webdemo/userpanel");
+        navigate("userpanel");
     } else { return null }
   }
 
   function userIsLoggedIn(loginStatus) {
     if (loginStatus === true) {
-      setguestButtons("none")
-      setuserButtons("initial")
+      setguestButtons('none');
+      setuserButtons('initial');
+      localStorage.setItem('loginStatus', 'true');
     } else {
-      setguestButtons("initial")
-      setuserButtons("none")
+      setguestButtons('initial');
+      setuserButtons('none');
+      localStorage.removeItem('loginStatus');
     }
   }
 
@@ -64,17 +74,17 @@ function WebNav() {
           <NavLink onClick={onCloseNav} to={""}><Image fit="contain" w="225px" h="60px" minWidth="225px" minHeight="60px" src={require("../../assets/images/logo02.png")} /></NavLink>
         </Flex>
         <Spacer />
-        <HStack justify="right" w="50%" spacing='1.5em' display={{base: 'none', md: 'flex', lg: 'flex'}} >
+        <HStack justify="right" w="50%" spacing='1.5em' display={{base: 'none', md: 'none', lg: 'flex'}} >
           <NavLink style={({ isActive }) => { return isActive? { color: "gold", fontWeight: "bold" } : {} }} to={"booking"}>APPOINTMENTS</NavLink>
           <NavLink style={({ isActive }) => { return isActive? { color: "gold", fontWeight: "bold" } : {} }} to={"renting"}>RENTALS</NavLink>
           <NavLink style={({ isActive }) => { return isActive? { color: "gold", fontWeight: "bold" } : {} }} to={"about-us"}>ABOUT</NavLink>
           <NavLink style={({ isActive }) => { return isActive? { color: "gold", fontWeight: "bold" } : {} }} to={"contact"}>CONTACT</NavLink>
-          <Box display={userButtons}><NavLink style={({ isActive }) => { return isActive? { color: "gold", fontWeight: "bold" } : {} }} to={"webdemo/userpanel"}>DASHBOARD</NavLink></Box>
+          <Box display={userButtons}><NavLink style={({ isActive }) => { return isActive? { color: "gold", fontWeight: "bold" } : {} }} to={"userpanel"}>DASHBOARD</NavLink></Box>
         </HStack>
         <Spacer />
-        <Box display={guestButtons}><Button onClick={onToggleLogin} display={{base: 'none', md: 'flex', lg: 'flex'}} variant='outline' colorScheme='twitter' size='lg' marginRight="1.5em" >LOG IN</Button></Box>
-        <Box display={userButtons}><Button onClick={logoutProcedure} display={{base: 'none', md: 'flex', lg: 'flex'}} variant='outline' colorScheme='red' size='lg' marginRight="1.5em" >LOG OUT</Button></Box>
-        <NavLink onClick={onToggleNav}><Button display={{base: 'flex', md: 'none', lg: 'none'}} variant='outline' colorScheme='twitter' size='lg' marginRight="1.5em" ><FontAwesomeIcon icon={faBars} /></Button></NavLink>
+        <Box display={guestButtons}><Button onClick={onToggleLogin} display={{base: 'none', md: 'none', lg: 'flex'}} variant='outline' colorScheme='twitter' size='lg' marginRight="1.5em" >LOG IN</Button></Box>
+        <Box display={userButtons}><Button onClick={logoutProcedure} display={{base: 'none', md: 'none', lg: 'flex'}} variant='outline' colorScheme='red' size='lg' marginRight="1.5em" >LOG OUT</Button></Box>
+        <NavLink onClick={onToggleNav}><Button display={{base: 'flex', md: 'flex', lg: 'none'}} variant='outline' colorScheme='twitter' size='lg' marginRight="1.5em" ><FontAwesomeIcon icon={faBars} /></Button></NavLink>
       </Flex>
       <Collapse in={isOpenNav} animateOpacity>
         <Box bg="white" padding="1.5em" rounded='md' shadow='md'><VStack spacing='1.5em' >
@@ -90,8 +100,11 @@ function WebNav() {
           <NavLink to={"contact"}>
             <Button width="300px" onClick={onToggleNav} colorScheme='messenger' size='lg'>CONTACT</Button>
           </NavLink>
-          <Box display={guestButtons}><Button width="300px" onClick={onToggleLogin} colorScheme='facebook' size='lg'>EMPLOYEE ACCESS</Button></Box>
-          <Box display={userButtons}><Button width="300px" onClick={logoutProcedure} colorScheme='red' size='lg'>LOG OUT</Button></Box>
+          <Button display={guestButtons} width="300px" onClick={onToggleLogin} colorScheme='facebook' size='lg'>EMPLOYEE ACCESS</Button>
+          <NavLink to={"userpanel"}>
+            <Button display={userButtons} width="300px" onClick={onToggleNav} colorScheme='facebook' size='lg'>DASHBOARD</Button>
+          </NavLink>
+          <Button display={userButtons} width="300px" onClick={logoutProcedure} colorScheme='red' size='lg'>LOG OUT</Button>
         </VStack></Box>
       </Collapse>
       </nav>
@@ -100,12 +113,13 @@ function WebNav() {
         <DrawerContent>
           <DrawerHeader borderBottomWidth='3px'>Employee area</DrawerHeader>
           <DrawerBody padding="1.5em">
-            <FormControl>
+          <Image marginLeft="1.2em" fit="contain" w="225px" h="60px" minWidth="225px" minHeight="60px" src={require("../../assets/images/logo01.png")} />
+            <FormControl marginTop="1.5em">
               <FormLabel>Email address</FormLabel>
-                <Input isRequired="true" type='email' onChange={(e => setUsername(e.target.value))} />
+                <Input marginBottom="1em" value={email} type='email' placeholder='type anything' id="email" onChange={(e => setEmail(e.target.value))} />
               <FormLabel>Password</FormLabel>
-                <Input isRequired="true" type='password' onChange={(e => setPassword(e.target.value))} />
-              <Button marginTop="1.5em" width="100%" type="submit" colorScheme='facebook' size='lg' onClick={handleLoginSubmit}>LOG IN</Button>
+                <Input value={password} type='password' placeholder='type anything' id="password" onChange={(e => setPassword(e.target.value))} />
+              <Button marginTop="2em" width="100%" type="submit" colorScheme='facebook' size='lg' onClick={handleLoginSubmit}>LOG IN</Button>
             </FormControl>
           </DrawerBody>
         </DrawerContent>
