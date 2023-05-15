e97152f3deac356b4eefcb4264410a5c0bc179e3
rodrigohrzm <rherranzm@gmail.com>
2023-05-15 20:18:09 +0200

    basic hero image

--- src/pages/webdemo/WebCallToAction.js
+++ src/pages/webdemo/WebCallToAction.js
@@ -1,13 +1,16 @@
 import { NavLink } from "react-router-dom"
 
+import { Box, VStack, Text, Heading, Button } from '@chakra-ui/react'
+
 function WebCallToAction() {
     return (
-      <>
-        <h1>The one-stop-shop for all your automotive needs</h1>
-        <p>Whether you need a routine maintenance check or a major repair, our experienced mechanics will take care of your vehicle and get you back on the road quickly and safely. Additionally, we offer a range of high-quality rental cars for those times when you need a temporary ride. With competitive pricing and exceptional customer service, we're committed to making sure you have the best experience possible.</p>
-        <button><NavLink to={"booking"}>Book an appointment</NavLink></button>
-        <img />
-      </>
+      <Box h='900px' bgSize='100%' backgroundPosition='cover' backgroundRepeat="no-repeat" backgroundImage={require("../../assets/images/hero.jpg")}>
+        <VStack align='left' spacing='2rem' paddingLeft='15rem' paddingTop='20rem' >
+          <Heading w='35%' color='white' as='h2' size='3xl'>The one-stop-shop for your car</Heading>
+          <Text w='30%' fontSize='lg' color='white'>We'll take care of your maintenance or major repairs and get you back on the road quickly and safely. And if you need a temporary ride, our range of high-quality rental cars is only a call away.</Text>
+          <Button variant='outline' width="250px" color='white' _hover={{ bg: '#f6f6f640' }} size='lg'><NavLink to={"booking"}>Book an appointment</NavLink></Button>
+        </VStack>
+      </Box>
     )};
 
   export { WebCallToAction };
