11dfcaf2e756509a9528d069c62bd28c6b02cbd4
rodrigohrzm <rherranzm@gmail.com>
2023-05-31 21:46:12 +0200

    working navigation

---
+++ src/pages/dashboard/DashboardOLD.js
@@ -0,0 +1,144 @@
+
+
+import { ChakraProvider, Portal, useDisclosure } from '@chakra-ui/react';
+//import Configurator from 'components/Configurator/Configurator';
+// Layout components
+//import AdminNavbar from 'components/Navbars/AdminNavbar.js';
+import DBsidebar from './elements/DBsidebar';
+import React, { useState } from 'react';
+import { useNavigate, Route, Routes } from 'react-router-dom';
+// Custom Chakra theme
+//import theme from 'theme/theme.js';
+import FixedPlugin from './elements/FixedPlugin';
+// Custom components
+import PanelMain from './elements/PanelMain';
+import PanelContainer from './elements/PanelContainer';
+import PanelContent from './elements/PanelContent';
+
+import { DBcontent } from './DBcontent';
+import { DBtables } from './DBtables';
+import { DBprofile } from './DBprofile';
+import { DBbilling } from './DBbilling';
+
+export function DashboardOLD(props) {
+
+
+	const { ...rest } = props;
+	// states and functions
+	const [ sidebarVariant, setSidebarVariant ] = useState('transparent');
+	const [ fixed, setFixed ] = useState(false);
+	const navigate = useNavigate();
+	// functions for changing the states from components
+	const getRoute = () => {
+		return window.location.pathname !== '/admin/full-screen-maps';
+	};
+	const getActiveRoute = (routes) => {
+		let activeRoute = 'Default Brand Text';
+		for (let i = 0; i < routes.length; i++) {
+			if (routes[i].collapse) {
+				let collapseActiveRoute = getActiveRoute(routes[i].views);
+				if (collapseActiveRoute !== activeRoute) {
+					return collapseActiveRoute;
+				}
+			} else if (routes[i].category) {
+				let categoryActiveRoute = getActiveRoute(routes[i].views);
+				if (categoryActiveRoute !== activeRoute) {
+					return categoryActiveRoute;
+				}
+			} else {
+				if (window.location.href.indexOf(routes[i].layout + routes[i].path) !== -1) {
+					return routes[i].name;
+				}
+			}
+		}
+		return activeRoute;
+	};
+	// This changes navbar state(fixed or not)
+	const getActiveNavbar = (routes) => {
+		let activeNavbar = false;
+		for (let i = 0; i < routes.length; i++) {
+			if (routes[i].category) {
+				let categoryActiveNavbar = getActiveNavbar(routes[i].views);
+				if (categoryActiveNavbar !== activeNavbar) {
+					return categoryActiveNavbar;
+				}
+			} else {
+				if (window.location.href.indexOf(routes[i].layout + routes[i].path) !== -1) {
+					if (routes[i].secondaryNavbar) {
+						return routes[i].secondaryNavbar;
+					}
+				}
+			}
+		}
+		return activeNavbar;
+	};
+	const getRoutes = (routes) => {
+		return routes.map((prop, key) => {
+			if (prop.collapse) {
+				return getRoutes(prop.views);
+			}
+			if (prop.category === 'account') {
+				return getRoutes(prop.views);
+			}
+			if (prop.layout === '/dashboard') {
+				//return <Route path={prop.layout + prop.path} component={prop.component} key={key} />;
+			} else {
+				return null;
+			}
+		});
+	};
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	document.documentElement.dir = 'ltr';
+	// Chakra Color Mode
+	return (
+		<>
+			<DBsidebar
+				routes={routes}
+				logoText={'PURITY UI DASHBOARD'}
+				display='none'
+				sidebarVariant={sidebarVariant}
+				{...rest}
+			/>
+			<PanelMain
+				w={{
+					base: '100%',
+					xl: 'calc(100% - 275px)'
+				}}>
+				<Portal>
+{/* 					<AdminNavbar
+						onOpen={onOpen}
+						logoText={'PURITY UI DASHBOARD'}
+						brandText={getActiveRoute(routes)}
+						secondary={getActiveNavbar(routes)}
+						fixed={fixed}
+						{...rest}
+					/> */}
+				</Portal>
+				{getRoute() ? (
+					<PanelContent>
+						<PanelContainer>
+							{/* <Routes> */}
+								{/* {getRoutes(routes)} */}
+								{/* <Navigate from='/admin' to='/admin/dashboard' /> */}
+							{/* </Routes> */}
+						</PanelContainer>
+					</PanelContent>
+				) : null}
+{/* 				<Portal>
+					<FixedPlugin secondary={getActiveNavbar(routes)} fixed={fixed} onOpen={onOpen} />
+				</Portal> */}
+{/* 				<Configurator
+					secondary={getActiveNavbar(routes)}
+					isOpen={isOpen}
+					onClose={onClose}
+					isChecked={fixed}
+					onRoutes={(value) => {
+						setFixed(value);
+					}}
+					onOpaque={() => setSidebarVariant('opaque')}
+					onTransparent={() => setSidebarVariant('transparent')}
+				/> */}
+			</PanelMain>
+		</>
+	);
+}
--- src/pages/dashboard/elements/DBsidebarContent.js
+++ src/pages/dashboard/elements/DBsidebarContent.js
@@ -1,249 +1,145 @@
-/*eslint-disable*/
-// chakra imports
-import {
-    Box,
-    Button, Flex,
-    Link,
-    Stack,
-    Text,
-    useColorModeValue
-} from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { useLocation, NavLink } from 'react-router-dom';
 import IconBox from "./IconBox";
-//import { CreativeTimLogo } from "components/Icons/Icons";
-
-
-import { DBcontent } from '../DBcontent';
-import { DBtables } from '../DBtables';
-import { DBprofile } from '../DBprofile';
-import { DBbilling } from '../DBbilling';
-import { Dashboard } from '../Dashboard';
-
-import { Separator } from "./Separator";
 import React from "react";
-import { NavLink, useLocation } from "react-router-dom";
 
-// this function creates the links and collapses that appear in the sidebar (left menu)
+const DBsidebarContent = () => {
 
+  let location = useLocation();
 
-const DBsidebarContent = ({ logoText, routes }) => {
+  const activeRoute = (routeName) => {
+    return location.pathname === routeName ? "active" : "";
+  };
 
   var routes = [
 		{
-		  path: "/overview",
+		  path: "",
 		  name: "Dashboard",
-		  component: DBcontent,
+		  component: <IconBox/>,
 		  layout: "/dashboard",
 		},
 		{
 		  path: "/projects",
 		  name: "Projects",
-		  component: DBtables,
+		  component: <IconBox/>,
 		  layout: "/dashboard",
+		},
+		{
+      path: "/bookings",
+      name: "Bookings",
+      component: <IconBox/>,
+      layout: "/dashboard",
 		},
 		{
 		  path: "/earnings",
 		  name: "Earnings",
-		  component: DBbilling,
+		  component: <IconBox/>,
 		  layout: "/dashboard",
-		},
-		{
-		  name: "ACCOUNT PAGES",
-		  category: "account",
-		  state: "pageCollapse",
-		  views: [
-			{
-			  path: "/bookings",
-			  name: "Profile",
-			  secondaryNavbar: true,
-			  component: DBprofile,
-			  layout: "/dashboard",
-			},
-		  ],
-		},
+		}
 	  ];
 
-    // to check for active links and opened collapses
-  let location = useLocation();
-  // this is for the rest of the collapses
-  const [state, setState] = React.useState({});
-
-  // verifies if routeName is the one active (in browser input)
-  const activeRoute = (routeName) => {
-    return location.pathname === routeName ? "active" : "";
-  };
-  const createLinks = (routes) => {
-    // Chakra Color Mode
-    const activeBg = useColorModeValue("white", "gray.700");
-    const inactiveBg = useColorModeValue("white", "gray.700");
-    const activeColor = useColorModeValue("gray.700", "white");
-    const inactiveColor = useColorModeValue("gray.400", "gray.400");
-
-    return routes.map((prop, key) => {
-      if (prop.redirect) {
-        return null;
-      }
-      if (prop.category) {
-        var st = {};
-        st[prop["state"]] = !state[prop.state];
-        return (
-          <div key={prop.name}>
-            <Text
-              color={activeColor}
-              fontWeight="bold"
-              mb={{
-                xl: "12px",
-              }}
-              mx="auto"
-              ps={{
-                sm: "10px",
-                xl: "16px",
-              }}
-              py="12px"
-            >
-              {document.documentElement.dir === "rtl"
-                ? prop.rtlName
-                : prop.name}
-            </Text>
-            {createLinks(prop.views)}
-          </div>
-        );
-      }
+  const createLinks = routes.map((prop, key) => {
       return (
         <NavLink to={prop.layout + prop.path} key={prop.name}>
-          {activeRoute(prop.layout + prop.path) === "active" ? (
-            <Button
-              boxSize="initial"
-              justifyContent="flex-start"
-              alignItems="center"
-              bg={activeBg}
-              mb={{
-                xl: "12px",
-              }}
-              mx={{
-                xl: "auto",
-              }}
-              ps={{
-                sm: "10px",
-                xl: "16px",
-              }}
-              py="12px"
-              borderRadius="15px"
-              _hover="none"
-              w="100%"
-              _active={{
-                bg: "inherit",
-                transform: "none",
-                borderColor: "transparent",
-              }}
-              _focus={{
-                boxShadow: "none",
-              }}
-            >
-              <Flex>
-                {typeof prop.icon === "string" ? (
-                  <Icon>{prop.icon}</Icon>
-                ) : (
-                  <IconBox
-                    bg="teal.300"
-                    color="white"
-                    h="30px"
-                    w="30px"
-                    me="12px"
-                  >
-                    {prop.icon}
-                  </IconBox>
-                )}
-                <Text color={activeColor} my="auto" fontSize="sm">
-                  {document.documentElement.dir === "rtl"
-                    ? prop.rtlName
-                    : prop.name}
-                </Text>
-              </Flex>
-            </Button>
-          ) : (
-            <Button
-              boxSize="initial"
-              justifyContent="flex-start"
-              alignItems="center"
-              bg="transparent"
-              mb={{
-                xl: "12px",
-              }}
-              mx={{
-                xl: "auto",
-              }}
-              py="12px"
-              ps={{
-                sm: "10px",
-                xl: "16px",
-              }}
-              borderRadius="15px"
-              _hover="none"
-              w="100%"
-              _active={{
-                bg: "inherit",
-                transform: "none",
-                borderColor: "transparent",
-              }}
-              _focus={{
-                boxShadow: "none",
-              }}
-            >
-              <Flex>
-                {typeof prop.icon === "string" ? (
-                  <Icon>{prop.icon}</Icon>
-                ) : (
-                  <IconBox
-                    bg={inactiveBg}
-                    color="teal.300"
-                    h="30px"
-                    w="30px"
-                    me="12px"
-                  >
-                    {prop.icon}
-                  </IconBox>
-                )}
-                <Text color={inactiveColor} my="auto" fontSize="sm">
-                  {document.documentElement.dir === "rtl"
-                    ? prop.rtlName
-                    : prop.name}
-                </Text>
-              </Flex>
-            </Button>
-          )}
-        </NavLink>
-      );
-    });
-  };
-
-    const links = <>{createLinks(routes)}</>;
-
-  return (
-    <>
-        <Box pt={"25px"} mb="12px">
-      <Link
-        href={`${process.env.PUBLIC_URL}/#/`}
-        target="_blank"
-        display="flex"
-        lineHeight="100%"
-        mb="30px"
-        fontWeight="bold"
-        justifyContent="center"
-        alignItems="center"
-        fontSize="11px"
-      >
-        {/* <CreativeTimLogo w="32px" h="32px" me="10px" /> */}
-        <Text fontSize="sm" mt="3px">
-          {logoText}
-        </Text>
-      </Link>
-      <Separator></Separator>
-    </Box>
-          <Stack direction="column" mb="40px">
-            <Box>{links}</Box>
-          </Stack>
-    </>
-  )
+        {activeRoute(prop.layout + prop.path) === "active" ? (
+          <Button
+            boxSize="initial"
+            justifyContent="flex-start"
+            alignItems="center"
+            bg="white"
+            mb={{
+              xl: "12px",
+            }}
+            mx={{
+              xl: "auto",
+            }}
+            ps={{
+              sm: "10px",
+              xl: "16px",
+            }}
+            py="12px"
+            borderRadius="8px"
+            _hover="none"
+            w="100%"
+            _active={{
+              bg: "inherit",
+              transform: "none",
+              borderColor: "transparent",
+            }}
+            _focus={{
+              boxShadow: "none",
+            }}
+          >
+            <Flex>
+              {typeof prop.icon === "string" ? (
+//                  <Icon>{prop.icon}</Icon>
+                <Text>Icon goes here</Text>
+              ) : (
+                <IconBox
+                  bg="teal.300"
+                  color="white"
+                  h="30px"
+                  w="30px"
+                  me="12px"
+                >
+                  {prop.icon}
+                </IconBox>
+              )}
+            </Flex>
+            <Text color='black'>{prop.name}</Text>
+          </Button>
+        ) : (
+          <Button
+            boxSize="initial"
+            justifyContent="flex-start"
+            alignItems="center"
+            bg="transparent"
+            mb={{
+              xl: "12px",
+            }}
+            mx={{
+              xl: "auto",
+            }}
+            py="12px"
+            ps={{
+              sm: "10px",
+              xl: "16px",
+            }}
+            borderRadius="8px"
+            _hover="none"
+            w="100%"
+            _active={{
+              bg: "inherit",
+              transform: "none",
+              borderColor: "transparent",
+            }}
+            _focus={{
+              boxShadow: "none",
+            }}
+          >
+            <Flex>
+              {typeof prop.icon === "string" ? (
+//                  <Icon>{prop.icon}</Icon>
+                <Text>Icon goes here</Text>
+              ) : (
+                <IconBox
+                  bg="white"
+                  color="teal.300"
+                  h="30px"
+                  w="30px"
+                  me="12px"
+                >
+                  {prop.icon}
+                </IconBox>
+              )}
+            </Flex>
+            <Text color='white'>{prop.name}</Text>
+          </Button>
+        )}
+      </NavLink>
+    )});
+  ;
+  return createLinks;
 }
 
 export default DBsidebarContent
